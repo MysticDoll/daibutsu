f816b0cffe1f490f83c13f3d664f892e62506f06
MysticDoll <mysticdoll@mysticdoll.com>
2015-10-13 13:04:39 +0900

    commit for azure

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 var gulp = require('gulp');
 var open = require('gulp-open');
-var server = require('./server');
 
 gulp.task('default', ['server', 'browser']);
 
-gulp.task('server', server());
+gulp.task('server', function(){require("./server");});
 
 gulp.task('browser', function(){
   gulp.src('./index.html')
--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-module.exports = function(){
-  var http = require('http');
+var http = require('http');
 
-  var url = require('url');
-  var fs = require('fs');
-  var port = process.env.PORT || 8080;
+var url = require('url');
+var fs = require('fs');
+var port = process.env.PORT || 8080;
 
-  var HttpServer = http.createServer(function(req, res){
-    var furl = req.url === "/" ? "index.html" : url.parse(req.url, true).pathname;
-    if(req.method === 'GET'){
-      fs.readFile("./" + furl, 'utf8', function(err, data){
-        if(!err){
-          if(/\.svg/.test(furl)){
-            res.writeHead(200, {'Content-Type': 'image/svg+xml'});
-          } else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-          }
-          res.end(data);
+var HttpServer = http.createServer(function(req, res){
+  var furl = req.url === "/" ? "index.html" : url.parse(req.url, true).pathname;
+  if(req.method === 'GET'){
+    fs.readFile("./" + furl, 'utf8', function(err, data){
+      if(!err){
+        if(/\.svg/.test(furl)){
+          res.writeHead(200, {'Content-Type': 'image/svg+xml'});
         } else {
-          res.writeHead(404, {'Content-Type': 'text/html'});
-          res.end("not found");
+          res.writeHead(200, {'Content-Type': 'text/html'});
         }
-      });
-    }
-  }).listen(port);
-}
+        res.end(data);
+      } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end("not found");
+      }
+    });
+  }
+}).listen(port);
